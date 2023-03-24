c44ada4af9feab4ce686e0084bbeb1c40197d012
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2023-03-24 07:07:07 +0500

    Create umumiy_vazifalar.py

---
+++ umumiy_vazifalar.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 24 06:52:37 2023
+
+@author: xdamir
+"""
+
+kocha = input("Ko'chani kiriting: ")
+mahalla = input("Mahallani kiriting: ")
+tuman = input("Tumanni kiriting: ")
+viloyat = input("Viloyatni kiriting: ")
+
+manzil = f"{kocha} ko\'chasi {mahalla} mahallasi {tuman} tumani {viloyat} viloyati"
+
+print(manzil)
+
+print(f"{kocha} ko\'chasi, \n{mahalla} mahallasi, \n{tuman} tumani, \n{viloyat} viloyati")
