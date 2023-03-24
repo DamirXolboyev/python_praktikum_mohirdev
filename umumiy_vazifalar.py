3a9107063126c14a35e2a5db30722158ac86be9c
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2023-03-24 07:12:58 +0500

    Update umumiy_vazifalar.py

--- umumiy_vazifalar.py
+++ umumiy_vazifalar.py
@@ -12,6 +12,17 @@ viloyat = input("Viloyatni kiriting: ")
 
 manzil = f"{kocha} ko\'chasi {mahalla} mahallasi {tuman} tumani {viloyat} viloyati"
 
+katta_harf = manzil.upper()
+
+kichkina_harf = manzil.lower()
+
+bosh_harf_katta = manzil.title()
+
+faqat_bosh_harf_katta = manzil.capitalize()
+
 print(manzil)
 
+print(katta_harf, kichkina_harf, bosh_harf_katta, faqat_bosh_harf_katta)
+
 print(f"{kocha} ko\'chasi, \n{mahalla} mahallasi, \n{tuman} tumani, \n{viloyat} viloyati")
+
