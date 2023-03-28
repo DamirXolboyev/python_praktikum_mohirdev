3e85d128a97497e0e66a50df0ecc9b32acc103ad
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2023-03-28 15:13:56 +0500

    hatolar to'g'irlandi

--- umumiy_vazifalar.py
+++ umumiy_vazifalar.py
@@ -42,13 +42,19 @@ Created on Fri Mar 24 06:52:37 2023
 
 # print(f"Siz {t_yil} da tug\'ilgansiz")
 
-son_1 = float(input("Birinchi sonni kiriting: "))
+#son_1 = float(input("Birinchi sonni kiriting: "))
 
-son_2 = float(input("Ikkinchi sonni kiriting: "))
+#son_2 = float(input("Ikkinchi sonni kiriting: "))
 
-qoshish = son_1 + son_2
-ayirish = son_1 - son_2
-kopaytirish = son_1 * son_2
-bolish = son_1 / son_2
+#qoshish = son_1 + son_2
+#ayirish = son_1 - son_2
+#kopaytirish = son_1 * son_2
+#bolish = son_1 / son_2
 
-print(f"{son_1} + {son_2} = {qoshish}\n {son_1} - {son_2} = {ayirish}\n {son_1} * {son_2} = {kopaytirish}\n {son_1} + {son_2} = {bolish}\n")
+#print(f"{son_1} + {son_2} = {qoshish}\n {son_1} - {son_2} = {ayirish}\n {son_1} * {son_2} = {kopaytirish}\n {son_1} + {son_2} = {bolish}\n")
+
+son = int(input("Istalgan butun son kiriting: "))
+
+for n in range(2,11):
+    if not (son%n):
+        print(f"{son} soni {n} ga qoldiqsiz bo'linadi")
