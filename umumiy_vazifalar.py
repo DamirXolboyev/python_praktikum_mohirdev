562cd96986c6c0deb741539a1ef5c4661ba1fab2
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2023-06-17 15:33:13 +0500

    funksiya dars vazifa

--- umumiy_vazifalar.py
+++ umumiy_vazifalar.py
@@ -492,19 +492,61 @@ FUNKSIYALAR
 # kvadrati(son)
 
 
+# def avto_info (kompaniya, model, rangi, korobka, yili, narhi=None):
+#     avto = { 'kompaniya': kompaniya,
+#              'model': model, 
+#              'rangi': rangi,
+#              'korobka': korobka,
+#              'yil': yili,
+#              'narh': narhi      
+#         }
+#     return avto
+
+# avto1 = avto_info('GM', 'Malibu', 'Qora', 'Avtomat', 2018)
+# avto2 = avto_info('GM','Gentra', 'Oq', 'Mexanika', 2016, 15000)
+# avtolar = [avto1, avto2]
+# print('Onlayn bozordagi mavjud avtomashinalar: ')
+# for avto in avtolar:
+#     if avto['narh']:
+#         narh = avto['narh']
+#     else:
+#         narh = "Noma'lum"
+#     print(f"{avto['rangi']} {avto['model']}. Narh {narh}")
 
 
 
+# def oraliq(min,max):
+#     sonlar = []
+#     while min < max:
+#         sonlar.append(min)
+#         min+=1
+#     return sonlar
 
 
 
+# print(oraliq(0,13))
+# print(oraliq(20,1651))
+
+"""
+dars davomida berilgan vazifa
+"""
+
+def oraliq_1(min,max,qadam=1):
+    sonlar = []
+    while min < max:
+        if qadam > 1:
+            sonlar.append(min)
+            min+=qadam
+        else:
+            sonlar.append(min)
+            min+=1
+    return sonlar
+    
 
 
+print(oraliq_1(23, 34, 2))
 
-
-
-
-
+print(oraliq_1(0,40))
 
 
 
